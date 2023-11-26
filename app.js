9c888592e9717058e3a0bc9e640e13d3e29e2361
r4coon77 <139823112+r4coon77@users.noreply.github.com>
2023-11-26 19:02:10 +0000

    Generated a 16x16 Gird with JavaScript

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 let canvasSelector = document.querySelector('#canvas');
 
+function createGrid() {
 
-function createGrid () {
-    for(let i = 0; i <= 16; i++) {
-    const divItem = document.createElement('div');
-    canvasSelector.appendChild(divItem);
+    for (let i = 0; i < 16; i++) {
+        let divRow = document.createElement('div');
+        divRow.className = "divRow";
+        canvasSelector.appendChild(divRow);
+        function createRow() {
+            for (let i = 0; i < 16; i++) {
+                const divItem = document.createElement('div');
+                divRow.appendChild(divItem);
+            }
+        }
+        createRow()
     }
+
 }
 
-createGrid ()
+createGrid()
