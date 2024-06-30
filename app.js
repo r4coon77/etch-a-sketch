a869210d9027edd834061c79b2d93d4f0727ee26
r4coon77 <139823112+r4coon77@users.noreply.github.com>
2024-06-30 21:58:11 +0100

    Added rest grid function and change grid size function. Placed drawing addEventListeners inside createGrid function in order to continue drawing after changing & reseting grid size

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const canvasSelector = document.querySelector('#canvas');
+let gridSize = 16;
+let = isDrawing = false;
 
-
-function createGrid() {
-
-    for (let i = 0; i < 16; i++) {
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
         const divRow = document.createElement('div');
         divRow.className = "divRow";
         canvasSelector.appendChild(divRow);
         function createRow() {
-            for (let i = 0; i < 16; i++) {
+            for (let i = 0; i < gridSize; i++) {
                 const divItem = document.createElement('div');
                 const btn = document.createElement('button');
                 divItem.className = "divItem";
@@ -17,49 +17,47 @@ function createGrid() {
         }
         createRow()
     }
-
+    let divItem = document.querySelectorAll('.divItem');
+    divItem.forEach(function(i) {
+        i.addEventListener('mousedown', () => {
+            isDrawing = true;
+            if(isDrawing === true){
+            console.log('Mousedown');
+            i.style.backgroundColor = 'blue';
+            } else console.log("Mouse not down")
+        }, false);
+        i.addEventListener('mouseover', () => {
+            if(isDrawing === true){
+            console.log('Mousedown');
+            i.style.backgroundColor = 'blue';
+            } else console.log("Mouse not down")
+        }, false);
+        i.addEventListener('mouseup', () =>{
+            isDrawing = false;
+        }, false);
+    });
+    canvasSelector.addEventListener('mouseleave', ()=>{
+        isDrawing = false;
+    })
 }
 
-createGrid()
+createGrid(gridSize)
 
 
-let divItem = document.querySelectorAll('.divItem');
-let = isDrawing = false;
-
-
-
-canvasSelector.addEventListener('mouseleave', ()=>{
-    isDrawing = false;
-})
-
-
-divItem.forEach(function(i) {
-    i.addEventListener('mousedown', () => {
-        isDrawing = true;
-        if(isDrawing === true){
-        console.log('Mousedown');
-        i.style.backgroundColor = 'blue';
-        } else console.log("Mouse not down")
-    }, false);
-    i.addEventListener('mouseover', () => {
-        if(isDrawing === true){
-        console.log('Mousedown');
-        i.style.backgroundColor = 'blue';
-        } else console.log("Mouse not down")
-    }, false);
-    i.addEventListener('mouseup', () =>{
-        isDrawing = false;
-    }, false);
-});
-
+function resetGrid() {
+    document.querySelectorAll('.divItem').forEach(e => e.remove());
+    document.querySelectorAll('.divRow').forEach(e => e.remove());
+}
 
 function changeGridSize() {
-    console.log("Hello, you clicked change gride size wahoo!")
+   resetGrid();
+   gridSize = prompt("Please Enter Gride Size  X By X", "0");
+   createGrid(gridSize);
+   console.log(gridSize)
 }
 
 
 
-changeGridSize()
 
 
 
